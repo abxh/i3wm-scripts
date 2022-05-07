3b32e31e166b0ef3dbf667d9cc6d0cd467b85bf1
abxh <83485102+abxh@users.noreply.github.com>
2022-05-08 01:19:26 +0200

    Add files via upload

---
+++ focused_window.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from i3ipc import Connection
+
+def get_focused_window_title(ipc):
+    return ipc.get_tree().find_focused().window_title
+
+
+def main():
+    ipc = Connection()
+    focused_window_title = get_focused_window_title(ipc)
+    if focused_window_title is not None:
+        print(focused_window_title)
+    else:
+        print('')
+
+if __name__ == "__main__":
+    main()
---
+++ focused_window_update.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from i3ipc import Connection,  Event
+import subprocess
+
+SIGNAL=10
+
+def update_title():
+    subprocess.run(["pkill","-SIGRTMIN+"+str(SIGNAL),"i3blocks"])
+
+def main():
+    ipc = Connection()
+
+    def on_window_change(ipc, e):
+        if e.change in ["focus","close","title"]:
+            update_title()
+    ipc.on(Event.WINDOW,on_window_change)
+    
+    def on_workspace_change(ipc, e):
+        if e.change == "focus":
+            update_title()
+    ipc.on(Event.WORKSPACE,on_workspace_change)
+
+    ipc.main()
+
+if __name__ == "__main__": 
+    main()
