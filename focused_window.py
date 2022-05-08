4721ceb7bef8ec919a6fac4129f1771d56cfafbe
abxh <83485102+abxh@users.noreply.github.com>
2022-05-08 11:04:43 +0200

    Update focused_window.py

--- focused_window.py
+++ focused_window.py
@@ -17,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+# Script to print focused window title to stdout.
+
 from i3ipc import Connection
 
 def get_focused_window_title(ipc):
